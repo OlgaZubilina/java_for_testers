9367274fa25ec439a41ff2ca3cf213f5a8028326
olga <zubilina.olga@gmail.com>
2024-06-09 12:08:50 +0300

    Задание 3

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -4,25 +4,30 @@ public class Triangle {
     double a;
     double b;
     double c;
-    
 
 
     public Triangle(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
+        if (a<=0 || b<=0 ||c<=0) {
+            throw new IllegalArgumentException("Сторона треугольника не может быть меньше или равна нулю");
+        }
+        if ((a+b)<=c ||(b+c)<=a ||(c+a)<=b){
+            throw new IllegalArgumentException("Сумма двух сторон треугольника не может быть меньше или равна третьей стороне");
+        }
 
     }
 
     public double perimeter() {
-double p =(this.a+this.b+this.c);
+        double p = (this.a + this.b + this.c);
         return p;
 
 
     }
 
     public double area() {
-        double S = Math.sqrt(((this.perimeter()/2) * ((this.perimeter()/2) - this.a) * ((this.perimeter()/2) - this.b) * ((this.perimeter()/2) - this.c)));
+        double S = Math.sqrt(((this.perimeter() / 2) * ((this.perimeter() / 2) - this.a) * ((this.perimeter() / 2) - this.b) * ((this.perimeter() / 2) - this.c)));
         return S;
 
     }
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -3,18 +3,110 @@ package ru.stqa.geometry.figures;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.text.DecimalFormat;
-
 public class TriangleTests {
-@Test
-    void canCalculatePerimeter (){
-var s = new Triangle(5.,6.,7.);
-double result = s.perimeter();
-        Assertions.assertEquals(18,result);}
-@Test
-void canCalculateArea(){
-    var s = new Triangle(5.0,6.0,7.0);
-double Area = s.area();
 
-    Assertions.assertEquals(15, Math.ceil(Area));
-}}
+    @Test // Тест на проверку вычисления периметра
+    void canCalculatePerimeter() {
+        var s = new Triangle(5., 6., 7.);
+        double result = s.perimeter();
+        Assertions.assertEquals(18, result);
+    }
+
+    @Test // Тест на проверку вычисления площади
+    void canCalculateArea() {
+        var s = new Triangle(5.0, 6.0, 7.0);
+        double Area = s.area();
+
+        Assertions.assertEquals(15, Math.ceil(Area));
+    }
+
+    @Test //Тест на исключение отрицательных сторон треугольника и сторон, равных нулю
+    void canValidateSide () {
+        try {
+            new Triangle(-1, 3, 4);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+        try {
+            new Triangle(3, -1, 4);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+        try {
+            new Triangle(5, 2, -1);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+        try {
+            new Triangle(0, 3, 4);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+        try {
+            new Triangle(3, 0, 4);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+        try {
+            new Triangle(5, 2, 0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+    }
+
+    @Test // Тест на проверку существования треугольника
+     void canTriangleExist () {
+         try {
+             new Triangle(3, 2, 1);
+             Assertions.fail();
+         } catch (IllegalArgumentException exception) {
+             //ok
+         }
+         try {
+             new Triangle(2, 3, 1);
+             Assertions.fail();
+         } catch (IllegalArgumentException exception) {
+             //ok
+         }
+         try {
+             new Triangle(2, 1, 3);
+             Assertions.fail();
+         } catch (IllegalArgumentException exception) {
+             //ok
+         }
+         try {
+             new Triangle(4, 2, 1);
+             Assertions.fail();
+         } catch (IllegalArgumentException exception) {
+             //ok
+         }
+         try {
+             new Triangle(2, 4, 1);
+             Assertions.fail();
+         } catch (IllegalArgumentException exception) {
+             //ok
+         }
+         try {
+             new Triangle(1, 2, 4);
+             Assertions.fail();
+         } catch (IllegalArgumentException exception) {
+             //ok
+         }
+     }
+
+}
+
+
+
+
+
+
+
+
+
