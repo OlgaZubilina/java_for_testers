bebdef642d43dd112c57624f256fdc4697af1bed
olga <zubilina.olga@gmail.com>
2024-06-16 13:04:16 +0300

    Создана модель групп

--- addressbook-web-tests/src/test/java/CreateGroup.java
+++ addressbook-web-tests/src/test/java/CreateGroup.java
@@ -1,74 +1,22 @@
-import org.junit.jupiter.api.BeforeEach;
+import model.GroupData;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CreateGroup {
-    private static WebDriver driver;
-
-
-    @BeforeEach
-    public void setUp() {
-        if (driver == null) {//проверка инициализации драйвера
-            driver = new ChromeDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(1076, 640));
-            driver.findElement(By.name("user")).click();
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).click();
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-
-    }
+public class CreateGroup extends TestBase {
 
 
     @Test
     public void canCreateGroup() {
-        if (!isElementPresent(By.name("new"))) {//переход в раздел группы при отсутствии кнопки "new group"
-            driver.findElement(By.linkText("groups")).click();
-        }
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("group_name");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("group_header");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("group_footer");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
+        openGroupsPage();
+        createGroup(new GroupData("name", "header", "footer"));
 
-    }
-
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
     }
 
     @Test
-    public void canCreateGroupEmpty() {
-        if (!isElementPresent(By.name("new"))) {//переход в раздел группы при отсутствии кнопки "new group"
-            driver.findElement(By.linkText("groups")).click();
-        }
-
-
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
+    public void canCreateGroupWithNameOnly() {
+        openGroupsPage();
+        var emptyGroup = new GroupData();
+        var groupWithName = emptyGroup.withName("some name");
+        createGroup(groupWithName);
 
     }
 }
