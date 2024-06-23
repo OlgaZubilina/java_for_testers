789585b6f87485679a4afd25ab73c8d546d11732
olga <zubilina.olga@gmail.com>
2024-06-23 18:12:05 +0300

    удаление контакта - падает в ошибку

--- addressbook-web-tests/src/test/java/tests/DeleteContact.java
+++ addressbook-web-tests/src/test/java/tests/DeleteContact.java
@@ -2,7 +2,12 @@ package tests;
 
 import model.ContactData;
 import model.GroupData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class DeleteContact extends TestBase {
 
@@ -11,7 +16,15 @@ public class DeleteContact extends TestBase {
         if (!app.contacts().isContactPresent()) {//создание нового контакта  при отсутствии
             app.contacts().createContact(new ContactData());
         }
-        app.contacts().deleteContact();
+        int contactCount = app.contacts().getCount();
+        List<ContactData> oldContacts = app.contacts().getList();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldContacts.size());
+        app.contacts().deleteContact(oldContacts.get(index));
+        List<ContactData> newContacts = app.contacts().getList();
+        var expectedList = new ArrayList<>(oldContacts);
+        expectedList.remove(index);
+        Assertions.assertEquals(newContacts, expectedList);
 
     }
 
