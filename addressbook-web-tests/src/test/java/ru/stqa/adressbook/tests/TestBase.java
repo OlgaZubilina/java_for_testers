5d90409ac5824126b568ceb33cfd51c97b4d90aa
olga <zubilina.olga@gmail.com>
2024-07-04 23:20:01 +0300

    Вынесено описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/adressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/adressbook/tests/TestBase.java
@@ -3,17 +3,24 @@ package ru.stqa.adressbook.tests;
 import ru.stqa.adressbook.manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestBase {
     protected static ApplicationManager app;
 
 
     @BeforeEach
-    public void setUp() {
-       if (app==null) {
-           app=new ApplicationManager();
-       }
-        app.init(System.getProperty("browser","firefox"));
+    public void setUp() throws IOException {
+        if (app == null) {
+            var properties = new Properties();
+            properties.load(new FileReader(System.getProperty("target", "local.properties")));
+            app = new ApplicationManager();
+            app.init(System.getProperty("browser", "firefox"), properties);
 
+        }
     }
-
 }
+
