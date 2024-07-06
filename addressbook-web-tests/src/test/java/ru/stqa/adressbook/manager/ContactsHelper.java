1df1ad5353db34c65c18ba347e874ecd733b2c6d
olga <zubilina.olga@gmail.com>
2024-07-06 14:22:17 +0300

    тесты для модификации групп и модификации контактов

--- addressbook-web-tests/src/test/java/ru/stqa/adressbook/manager/ContactsHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/adressbook/manager/ContactsHelper.java
@@ -2,6 +2,8 @@ package ru.stqa.adressbook.manager;
 
 import ru.stqa.adressbook.model.ContactData;
 import org.openqa.selenium.By;
+
+import javax.xml.xpath.XPath;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,13 +22,13 @@ public class ContactsHelper extends HelperBase {
 
     public void deleteContact(ContactData contact) {
         openContactsPage();
-        selectContact(contact);
+
         submitDeleteContact();
 
 
     }
 
-    public void updateContact(ContactData contact,ContactData updateContact) {
+    public void updateContact(ContactData contact, ContactData updateContact) {
         openContactsPage();
         //selectContact(contact);
         openContact(contact);
@@ -37,20 +39,16 @@ public class ContactsHelper extends HelperBase {
     }
 
     private void submitUpdateContact() {
-        click(By.cssSelector("input:nth-child(74)"));
+        click(By.name("update"));
     }
 
     private void openContact(ContactData contact) {
-        click(By.partialLinkText(String.format("edit.php?id=%s",contact.id())));
+        click(By.xpath(String.format("//td/a[contains(@href, 'edit.php?id=%s')]", contact.id())));
     }
 
 
     private void submitDeleteContact() {
         click(By.cssSelector(".left:nth-child(8) > input"));
-    }
-
-    private void selectContact(ContactData contact) {
-        click(By.cssSelector(String.format("input[value='%s']",contact.id())));
     }
 
 
@@ -72,48 +70,22 @@ public class ContactsHelper extends HelperBase {
 
     private void fillContactForm(ContactData contact) {
         type(By.name("firstname"), contact.firstname());
-        type(By.name("middlename"), contact.middlename());
+        //type(By.name("middlename"), contact.middlename());
         type(By.name("lastname"), contact.lastname());
-        type(By.name("nickname"), contact.nickname());
-        attach(By.name("photo"),contact.photo());
-        //driver.findElement(By.name("photo")).click();
-        // driver.findElement(By.name("photo")).sendKeys("C:\Users\Work\Desktop\скрины\Screenshot_1.png");
-        type(By.name("title"), contact.title());
-        type(By.name("company"), contact.company());
+        // type(By.name("nickname"), contact.nickname());
+        attach(By.name("photo"), contact.photo());
+        // type(By.name("title"), contact.title());
+        //type(By.name("company"), contact.company());
         type(By.name("address"), contact.address());
-        type(By.name("home"), contact.home());
-        type(By.name("mobile"), contact.mobile());
-        type(By.name("work"), contact.work());
-        type(By.name("fax"), contact.fax());
-        type(By.name("email"), contact.email());
-        type(By.name("email2"), contact.email2());
-        type(By.name("email3"), contact.email3());
-        type(By.name("homepage"), contact.homepage());
+        //type(By.name("home"), contact.home());
+        //type(By.name("mobile"), contact.mobile());
+        //type(By.name("work"), contact.work());
+        //type(By.name("fax"), contact.fax());
+        //type(By.name("email"), contact.email());
+        //type(By.name("email2"), contact.email2());
+        // type(By.name("email3"), contact.email3());
+        //type(By.name("homepage"), contact.homepage());
 
-        /*driver.findElement(By.name("bday")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("bday"));
-            dropdown.findElement(By.xpath("//option[. = '4']")).click();
-        }
-        // driver.findElement(By.name("bmonth")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("bmonth"));
-            dropdown.findElement(By.xpath("//option[. = 'February']")).click();
-        }
-        // driver.findElement(By.name("byear")).click();
-        //driver.findElement(By.name("byear")).sendKeys("123");
-        //driver.findElement(By.name("aday")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("aday"));
-            dropdown.findElement(By.xpath("//option[. = '1']")).click();
-        }
-        //driver.findElement(By.name("amonth")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("amonth"));
-            dropdown.findElement(By.xpath("//option[. = 'April']")).click();
-        }
-        //driver.findElement(By.name("ayear")).click();
-        driver.findElement(By.name("ayear")).sendKeys("123");*/
     }
 
     public void openContactCreatePage() {
@@ -135,16 +107,16 @@ public class ContactsHelper extends HelperBase {
         for (var tr : trs) {
             var tds = tr.findElements(By.cssSelector("td"));
             for (var td : tds) {
-               var check = td.findElement(By.name("selected[]"));
+                var check = td.findElement(By.name("selected[]"));
                 var id = check.getAttribute("value");
                 var lastname = tds.get(1).getText();
                 var firstname = tds.get(2).getText();
                 contacts.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
-            break;}
+                break;
+            }
 
 
-
-    }
+        }
         return contacts;
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/adressbook/tests/UpdateContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/adressbook/tests/UpdateContact.java
@@ -1,8 +1,9 @@
 package ru.stqa.adressbook.tests;
 
-import ru.stqa.adressbook.model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.stqa.adressbook.common.CommonFunctions;
+import ru.stqa.adressbook.model.ContactData;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -13,16 +14,16 @@ public class UpdateContact extends TestBase {
     @Test
     void canUpdateContact() {
    if(!app.contacts().isContactPresent()){
-    app.contacts().createContact(new ContactData("id","firstname", "middlename", "lastname", "nickname", "title", "company", "adress", "photo","home", "mobile", "work", "fax", "email", "email2", "email3", "homepage"));
+    app.contacts().createContact(new ContactData("id","firstname", "middlename", "lastname", "nickname", "title", "company", "adress", "","home", "mobile", "work", "fax", "email", "email2", "email3", "homepage"));
         }
         var oldContacts = app.contacts().getList();
         var rnd = new Random();
         var index = rnd.nextInt(oldContacts.size());
-        var testData = new ContactData().withFirstname("update firstname");
+        var testData = new ContactData().withFirstname("update firstname").withPhoto(CommonFunctions.randomFile("src/test/resources/images"));
         app.contacts().updateContact(oldContacts.get(index), testData);
         var newContacts = app.contacts().getList();
         var expectedList = new ArrayList<>(oldContacts);
-        expectedList.set(index,testData.withId(oldContacts.get(index).id()));
+        expectedList.set(index,testData.withId(oldContacts.get(index).id()).withAdress("").withPhoto(""));
         Comparator<ContactData> compareById = (o1, o2) -> {
             return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
         };
